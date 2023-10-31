3c6713d2a2ad36f1c0433f7eace1cded8da720d1
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-31 09:53:01 +0200

    man labaks kods neka jekabam

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import ToDo from "./ToDo.js";
+import React, { useEffect, useState } from "react";
 
 function App() {
-  const todo = {
-    userId: 1,
-    id: 1,
-    title: "delectus aut autem",
-    completed: false,
-  };
+  const [todo, setTodo] = useState({});
+  useEffect(() => {
+    async function getToDoData() {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos/69"
+      );
+      const doto = await response.json();
+      setTodo(doto);
+    }
+    getToDoData();
+  });
+
+  //  const todo = {
+  //    userId: 1,
+  //    id: 1,
+  //    title: "delectus aut autem",
+  //    completed: false,
+  //  };
 
   //const helloToDo = todo.map((index, current) => {
   //  return <ToDo key={index} />
